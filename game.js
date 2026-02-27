1cfaa6f0469f63ac0869d7401036c36b2c8204d3
Programmer Sam <81997712+Programmer1Sam@users.noreply.github.com>
2026-02-26 18:37:58 -0600

    added bumpers in board layout

--- game.js
+++ game.js
@@ -12,11 +12,25 @@ var render = Render.create({
     engine: engine
 });
 
-var bumperA = Bodies.circle(400, 200, 80, {isStatic: true}); // creates bumper that should be still, isStatic is nessesary 
+// creates bumper that should be still, isStatic is nessesary 
+var bumperA = Bodies.circle(200, 150, 40, {isStatic: true}); 
+var bumperB = Bodies.circle(500, 150, 40, {isStatic: true}); 
+var bumperC = Bodies.circle(350, 300, 40, {isStatic: true});
+var bumperD = Bodies.circle(250, 450, 40, {isStatic: true}); 
+var bumperE = Bodies.circle(450, 450, 40, {isStatic: true}); 
+var bumperF = Bodies.circle(350, 600, 40, {isStatic: true});
+var bumperG = Bodies.circle(550, 700, 40, {isStatic: true}); 
+var bumperH = Bodies.circle(150, 700, 40, {isStatic: true});
 
-Composite.add(engine.world, bumperA); 
+physicsObjs = [
+    bumperA,bumperB,bumperC,
+    bumperD,bumperE,bumperF,
+    bumperG,bumperH
+]
 
-Matter.Render.setSize(render, 728, 1024); // method to change render window size 
+Composite.add(engine.world, physicsObjs); 
+
+Matter.Render.setSize(render, 800, 1024); // method to change render window size 
 
 Render.run(render);
 
