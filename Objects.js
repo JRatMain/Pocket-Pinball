6ab8fa530b0fe265cf54bd4124972786d37f3bf5
Programmer Sam <81997712+Programmer1Sam@users.noreply.github.com>
2026-03-07 15:17:34 -0600

    Board layout has caught up with old game design, ready to have constraints and bumpers added

--- Objects.js
+++ Objects.js
@@ -1,5 +1,5 @@
 // function to create game ball
-function Ball(x, y, r) {
+function Ball(x, y, r, options, color = 255) {
     var options = {
     friction: 0.5,
     restitution: 0.8,
@@ -16,17 +16,121 @@ function Ball(x, y, r) {
         push();
         stroke(200);
         strokeWeight(2);
-        fill(0, 255, 255, 100);
+        fill(color);
         translate(pos.x, pos.y);
         rotate(angle);
-        ellipse(0, 0, this.r);
+        ellipse(0, 0, this.r * 2);
         pop();
 
     }
        
 }
 
-//Function to create rectangles, can be used for walls or boxes
-function modRectangle(x, y, w, h, options) {
+// function to create bumpers
+function Bumpers(x, y, r, color = 255) {
+    var options = {
+    friction: 0.5,
+    restitution: 0.8,
+    angle: Math.PI
+  }
+    this.body = Bodies.circle(x, y, r, {isStatic: true});
+    this.r = r;
+    World.add(world, this.body);
+    
+    this.show = function() {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        
+        push();
+        stroke(200);
+        strokeWeight(2);
+        fill(color);
+        translate(pos.x, pos.y);
+        rotate(angle);
+        ellipse(0, 0, this.r * 2);
+        pop();
 
+    }
+       
+}
+
+
+//Function to create rectangles, can be used for physics rectangles
+function modRectangle(x, y, w, h, options) {
+      var options = {
+    friction: 0.5,
+    restitution: 0.8,
+    angle: Math.PI
+  }
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+
+    World.add(world, this.body);
+    
+    this.show = function() {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        
+        push();
+        stroke(200);
+        strokeWeight(2);
+        fill(0, 255, 255, 100);
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rect(0, 0, this.w, this.h);
+        pop();
+
+    }
+}
+
+//Function to create static rectangles, good for walls
+function staticRect(x, y, w, h, options, color = 255) {
+      var options = {
+    friction: 0.5,
+    restitution: 0.8,
+    angle: Math.PI
+  }
+    this.body = Bodies.rectangle(x, y, w, h, {isStatic: true});
+    this.w = w;
+    this.h = h;
+
+    World.add(world, this.body);
+    
+    this.show = function() {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        
+        push();
+        stroke(200);
+        strokeWeight(2);
+        fill(color);
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rect(0, 0, this.w, this.h);
+        pop();
+
+    }
+}
+
+function staticTriangle(x1, y1, x2, y2, x3, y3, color) {
+    this.body = Bodies.fromVertices(0,0, [{x:x1, y:y1}, {x:x2, y:y2}, {x:x3, y:y3}], {isStatic: true});
+
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.x3 = x3;
+    this.y3 = y3;
+
+    World.add(world, this.body);
+
+    this.show = function() {
+        push();
+        stroke(200);
+        strokeWeight(2);
+        fill(color);
+        triangle(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3);
+        pop();
+    } 
 }
--- gameV2.js
+++ gameV2.js
@@ -10,20 +10,55 @@ var obstacles = [];
 var walls = [];
 var ground;
 var staticObj = {isStatic: true}
-
+var gameball;
 
 
 function setup() {
   createCanvas(800, 1024);
+
   engine = Engine.create(); 
   engine.world.gravity.y = 1;
   world = engine.world;
   Runner.run(engine);
+
   stroke(255);
   strokeWeight(5);
-    fill(0);
-  var gameball = new Ball(500, 500, 50);
-  walls.push(gameball);
+  fill(0);
+
+  gameball = new Ball(500, 500, 30);
+  
+  var bumperA = new Bumpers(200, 150, 30); 
+  var bumperB = new Bumpers(500, 150, 30); 
+  var bumperC = new Bumpers(350, 300, 30);
+  var bumperD = new Bumpers(250, 450, 30); 
+  var bumperE = new Bumpers(450, 450, 30); 
+  var bumperF = new Bumpers(350, 600, 30);
+  var bumperG = new Bumpers(550, 700, 30); 
+  var bumperH = new Bumpers(150, 700, 30);
+
+  var wallL = new staticRect(0, 0, 20, 1024);
+  var wallR = new staticRect(780, 0, 20, 1024);
+  var chuteBorderL = new staticRect(700, 150, 10, 1024);
+
+  var chuteAngle = new staticTriangle(700, 0, 780, 0, 780, 100, 0);
+
+  walls.push(wallL);
+  walls.push(wallR);
+  walls.push(chuteBorderL);
+  
+  walls.push(chuteAngle);
+
+  walls.push(bumperA);
+  walls.push(bumperB);
+  walls.push(bumperC);
+  walls.push(bumperD);
+  walls.push(bumperE);
+  walls.push(bumperF);
+  walls.push(bumperG);
+  walls.push(bumperH);
+
+
+
   World.add(world, walls);
     ground = Bodies.rectangle(200, height, width, 10, {isStatic: true});
   World.add(world, ground);
@@ -35,6 +70,7 @@ function draw() {
     
   for (var i = 0; i < walls.length; i++) {
     walls[i].show();
+    gameball.show();
   }
 
   stroke(255);
